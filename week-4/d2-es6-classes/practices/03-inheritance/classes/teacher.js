b74f2113b3558510529ba7bd5c9a1316b45f866c
Jun Park <junhyung96@gmail.com>
2023-04-11 15:43:50 -0400

    inheritance practice

--- week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
@@ -1,6 +1,20 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person{
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName, lastName);
+    this.subject = subject
+    this.yearsOfExperience = yearsOfExperience
+  }
+
+  static combinedYearsOfExperience(teachers){
+    let total = 0
+    for (let teacher of teachers){
+      total += teacher.yearsOfExperience
+    }
+    return total
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
