36134a4a42ecc1566b345f930c40b16bca648e76
Jun Park <junhyung96@gmail.com>
2023-04-12 13:48:07 -0400

    01-03 practices

--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,6 @@
-// Your code here
+const Employee = require('./employee');
+
+const newEmployee = new Employee('John Wick', 'Dog Lover');
+
+setTimeout(newEmployee.sayName.bind(newEmployee),2000);
+setTimeout(newEmployee.sayOccupation.bind(newEmployee),3000);
