f2183142581d570de49c4d9ad35537d3ac2db986
Jun Park <junhyung96@gmail.com>
2023-04-13 16:32:43 -0400

    02 unit testing prac

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,14 +1,30 @@
 function isFive(num) {
-  // Your code here
+  if (num === 5) return true;
+  return false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== "number") {
+    throw new Error();
+  }
+  if (number % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let newArr = [];
+  for (let i = min; i <= max; i += step){
+    newArr.push(i)
+  }
+  return newArr;
 }
 
+// expect(actual1).to.eql(expected1);
+// console.log(actual1) // .to.eql(expected1);
+// console.log(actual1) // (expected1)
+// console.log(myRange(min1, max1)) // [0, 1, 2, 3, 4, 5]
 
 module.exports = { isFive, isOdd, myRange };
--- week-4/d4-test-driven-development/practices/02-unit-testing/test/class-spec.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/test/class-spec.js
@@ -4,31 +4,42 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let appacademy;
+  let lunch;
+  beforeEach(()=>{
+    appacademy = new Word ('appacademy')
+    lunch = new Word('lunch')
+  })
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(appacademy).to.be.property('word');
+      expect(lunch).to.be.property('word');
     });
 
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(appacademy.word).to.eql("appacademy");
+      expect(lunch.word).to.eql('lunch');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(appacademy.removeVowels()).to.equal('ppcdmy');
+      expect(lunch.removeVowels()).to.equal('lnch');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(appacademy.removeConsonants()).to.equal('aaae');
+      expect(lunch.removeConsonants()).to.equal('u');
     });
   });
 
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(appacademy.pigLatin()).to.equal('appacademyyay')
+      expect(lunch.pigLatin()).to.equal('unchlay')
     });
   });
 });
