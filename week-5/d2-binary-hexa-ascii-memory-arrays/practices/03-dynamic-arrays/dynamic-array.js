b8e3cdae0d30d2c577a58fb33c0795cd1e877850
Jun Park <junhyung96@gmail.com>
2023-04-18 18:25:19 -0400

    03 practice

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -1,21 +1,29 @@
 class DynamicArray {
-
   constructor(defaultSize=4) {
-
-    // Your code here
-
+    this.length = 0;
+    this.data = new Array(defaultSize);
+    this.capacity = defaultSize;
   }
 
   read(index) {
-
-    // Your code here
+    if (index >= this.length) return undefined;
+    return this.data[index]
   }
 
   unshift(val) {
+    // this.length = val
+    // for (let i = this.data.length-1; i >= 0; --i) {
+    //   this.data[i + 1] = this.data[i];
+    // }
+    // this.data[0] = val;
 
-    // Your code here
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = val;
+    this.length++;
+    return this.length;
   }
-
 }
 
 
