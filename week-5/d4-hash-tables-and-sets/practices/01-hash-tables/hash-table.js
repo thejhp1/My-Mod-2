67960a97f9fc90e0d7533abf8b9e305c08400569
Jun Park <junhyung96@gmail.com>
2023-04-20 16:16:43 -0400

    hash table practice

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 class KeyValuePair {
   constructor(key, value) {
@@ -9,32 +9,93 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    // let dec = '0x' + sha256(key).slice(0,8)
+    // return parseInt(dec)
+    return parseInt(sha256(key).slice(0, 8), 16);
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    const newPair = new KeyValuePair(key, value);
+    const i = this.hashMod(key);
+    if (this.data[i]) {
+      throw new Error("hash collision or same key/value pair already exists!");
+    } else {
+      this.data[i] = newPair;
+    }
+    this.count++;
+    return this;
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    const newPair = new KeyValuePair(key, value);
+    const i = this.hashMod(key);
+    // if (this.data[i]){
+    //   let old = this.data[i];
+    //   this.data[i] = newPair;
+    //   this.data[i].next = old;
+    // } else {
+    //   this.data[i] = newPair;
+    // }
+    newPair.next = this.data[i];
+    this.data[i] = newPair;
+    this.count++;
+    return this;
   }
 
   insert(key, value) {
-    // Your code here
+    // const newPair = new KeyValuePair(key, value);
+    // const i = this.hashMod(key);
+    // if (!this.data[i]) {
+    //   this.data[i] = newPair;
+    // } else {
+    //   let current = this.data[i];
+    //   while (current) {
+    //     if (current.key === key) {
+    //       current.value = value;
+    //       return;
+    //     }
+    //     current = current.next;
+    //   }
+    //   newPair.next = this.data[i];
+    //   this.data[i] = newPair;
+    // }
+    // this.count++;
+
+    const i = this.hashMod(key);
+    let currKVP = this.data[i];
+
+    // while (currKVP) {
+    //   if (currKVP.key === key) {
+    //     break;
+    //   }
+    //   currKVP = currKVP.next;
+    // }
+
+    while (currKVP && currKVP.key !== key) {
+      currKVP = currKVP.next;
+    }
+
+    if (currKVP) {
+      currKVP.value = value;
+    } else {
+      const newPair = new KeyValuePair(key, value);
+      newPair.next = this.data[i];
+      this.data[i] = newPair;
+      this.count++;
+      return this;
+    }
   }
-
 }
-
 
 module.exports = HashTable;
