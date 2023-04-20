c86b55fceec715e2cef34612f714db711064b0d7
Jun Park <junhyung96@gmail.com>
2023-04-19 20:10:07 -0400

    d3 practice

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -8,15 +8,34 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+    newNode.next = this.head;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+    }
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+    newNode.prev = this.tail;
+    if (!this.head) {
+      this.head = newNode
+    } else {
+      this.tail.next = newNode
+    }
+    this.tail = newNode
+    this.length++
   }
 
   // You can use this function to help debug
