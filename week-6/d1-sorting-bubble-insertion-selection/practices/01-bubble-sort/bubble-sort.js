e04ff6d291f55c41a9c13a59b7a36e208ba2c698
Jun Park <junhyung96@gmail.com>
2023-04-24 16:59:41 -0400

    bubble sort prac

--- week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
@@ -1,18 +1,27 @@
-
 function bubbleSort(arr) {
-
+  debugger;
+  let swapped = false;
   // Iterate through the array
-
+  for (let i = 0; i < arr.length - 1; i++) {
     // If the current value is greater than its neighbor to the right
-      // Swap those values
-
+    // Swap those values
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
       // Do not move this console.log
       console.log(arr.join(","));
-
+      swapped = true;
+    };
+  }
   // If you get to the end of the array and no swaps have occurred, return
-
-  // Otherwise, repeat from the beginning
-
+  if (swapped === false) {
+    return arr;
+  } else {
+    // Otherwise, repeat from the beginning
+    return bubbleSort(arr);
+  }
 }
 
+arr = [2, 4, 6, 8, 1, 3, 5, 7, 9];
+// bubbleSort(arr)
+console.log(bubbleSort(arr));
 module.exports = bubbleSort;
