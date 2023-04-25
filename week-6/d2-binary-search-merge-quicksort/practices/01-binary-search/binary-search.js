c7d21e41e4f554d887cd06dcbd3a3f2f58d34338
Jun Park <junhyung96@gmail.com>
2023-04-25 12:19:04 -0400

    binary search practice

--- week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
+++ week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
@@ -1,6 +1,13 @@
 function linearSearch (arr, target) {
 
-  // Can you solve this in one line?
+  // for (let i = 0; i < arr.length; i++){
+  //   if (arr[i] === target){
+  //     return i
+  //   }
+  // }
+  // return -1
+
+  return arr.indexOf(target)
 
 };
 
@@ -22,8 +29,20 @@ function binarySearch(arr, target) {
       // Move the high pointer to midpoint - 1
 
   // Return -1 if the loop exits with overlapping pointers
-
+  let low = arr[0];
+  let high = arr[arr.length - 1];
+  while (low <= high) {
+    const middle = low + Math.floor((high - low) / 2);
+    if (target === arr[middle]){
+      return middle;
+    }
+    if (target > arr[middle]) {
+      low = middle + 1;
+    } else {
+      high = middle - 1;
+    }
+  }
+  return -1
 }
-
 
 module.exports = [linearSearch, binarySearch]
