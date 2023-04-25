65e1257b425c2c477bf0060bd9f380f665d91462
Jun Park <junhyung96@gmail.com>
2023-04-25 18:01:08 -0400

    merge and quick sort practice

--- week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
@@ -1,33 +1,44 @@
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
-
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  // If so, it's already sorted: return
+  if (arr.length < 2) return arr;
 
   // Divide the array in half
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0,mid);
+  const right = arr.slice(mid);
 
   // Recursively sort the left half
   // Recursively sort the right half
+  // let asd0 = mergeSort(left)
+  // let asd1= mergeSort(right)
+  // return merge(asd0,asd1)
 
   // Merge the halves together and return
-
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 
 // Takes in two sorted arrays and returns them merged into one
 function merge(arrA, arrB) {
-
   // Create an empty return array
+  const res = [];
 
-  // Point to the first value of each array
   // While there are still values in each array...
+  while(arrA.length && arrB.length){
     // Compare the first values of each array
     // Add the smaller value to the return array
-    // Move the pointer to the next value in that array
+    if(arrA[0] <= arrB[0]){
+      res.push(arrA.shift());
+    } else {
+      res.push(arrB.shift());
+    }
+  }
 
   // Return the return array
-
+  return [...res, ...arrA, ...arrB];
 }
 
 module.exports = [merge, mergeSort];
--- week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
@@ -1,19 +1,27 @@
 function quicksort(arr) {
-
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
+  if (arr.length < 2) return arr;
 
-  // Pick the first value as the pivot
+  // Pick the last value as the pivot
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
 
   // Orient the pivot so that...
       // every number smaller than the pivot is to the left
       // every number larger (or equal) than the pivot is to the right
-
+  for (let i = 0; i < arr.length - 1; i++){
+    if (arr[i] < pivot){
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
   // Recursively sort the left
   // Recursively sort the right
-
   // Return the left, pivot and right in sorted order
-
+  return [...quicksort(left), pivot, ...quicksort(right)]
 }
 
 
