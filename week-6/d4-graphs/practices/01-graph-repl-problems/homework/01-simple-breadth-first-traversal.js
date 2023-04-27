5508ba0f2275dff7896311a348eef3659a602972
Jun Park <junhyung96@gmail.com>
2023-04-27 13:10:56 -0400

    01 and 02 reformatted

--- week-6/d4-graphs/practices/01-graph-repl-problems/homework/01-simple-breadth-first-traversal.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/homework/01-simple-breadth-first-traversal.js
@@ -1,39 +1,39 @@
 const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5, 6],
-    5: [1, 2, 4],
-    6: [4]
-}
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5, 6],
+  5: [1, 2, 4],
+  6: [4],
+};
 
 function printBreadthFirst(start) {
-    const queue = []
-    queue.push(start);
-    const visited = new Set();
-    visited.add(start);
+  const queue = [];
+  queue.push(start);
+  const visited = new Set();
+  visited.add(start);
 
-    while (queue.length) {
-        const curr = queue.shift();
-        console.log(curr);
+  while (queue.length) {
+    const curr = queue.shift();
+    console.log(curr);
 
-        const neighbors = adjList[curr];
-        neighbors.forEach(neighbor => {
-            if (!visited.has(neighbor)) {
-                queue.push(neighbor);
-                visited.add(neighbor);
-            }
-        });
-    }
-    return;
+    const neighbors = adjList[curr];
+    neighbors.forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    });
+  }
+  return;
 }
 
-console.log("First Test:")
+console.log("First Test:");
 printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                    // One possible solution:  3, 2, 4, 1, 5, 6
-console.log("Second Test:")
+// One possible solution:  3, 2, 4, 1, 5, 6
+console.log("Second Test:");
 printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  6, 4, 3, 5, 2, 1
-console.log("Third Test:")
+// One possible solution:  6, 4, 3, 5, 2, 1
+console.log("Third Test:");
 printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-                    // One possible solution:  4, 3, 5, 6, 2, 1
+// One possible solution:  4, 3, 5, 6, 2, 1
--- week-6/d4-graphs/practices/01-graph-repl-problems/homework/02-simple-depth-first-traversal.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/homework/02-simple-depth-first-traversal.js
@@ -1,39 +1,39 @@
 const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5, 6],
-    5: [1, 2, 4],
-    6: [4]
-}
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5, 6],
+  5: [1, 2, 4],
+  6: [4],
+};
 
 function printDepthFirst(start) {
-    const stack = []
-    stack.push(start);
-    const visited = new Set();
-    visited.add(start);
+  const stack = [];
+  stack.push(start);
+  const visited = new Set();
+  visited.add(start);
 
-    while (stack.length) {
-        const curr = stack.pop();
-        console.log(curr);
+  while (stack.length) {
+    const curr = stack.pop();
+    console.log(curr);
 
-        const neighbors = adjList[curr];
-        neighbors.forEach(neighbor => {
-            if (!visited.has(neighbor)) {
-                stack.push(neighbor);
-                visited.add(neighbor);
-            }
-        });
-    }
-    return;
+    const neighbors = adjList[curr];
+    neighbors.forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        stack.push(neighbor);
+        visited.add(neighbor);
+      }
+    });
+  }
+  return;
 }
 
-console.log("First Test:")
+console.log("First Test:");
 printDepthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                    // One possible solution:  3, 4, 6, 5, 1, 2
-console.log("Second Test:")
+// One possible solution:  3, 4, 6, 5, 1, 2
+console.log("Second Test:");
 printDepthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  6, 4, 5, 2, 1, 3
-console.log("Third Test:")
+// One possible solution:  6, 4, 5, 2, 1, 3
+console.log("Third Test:");
 printDepthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-                    // One possible solution:  4, 6, 5, 2, 1, 3
+// One possible solution:  4, 6, 5, 2, 1, 3
